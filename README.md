15d65c42497cb59e1203d9c47527d39eced453a8
Ruben Maurer <rubenmaurer@live.de>
2019-05-09 18:24:12 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# MonsterBorgRemote
+# Thunder Remote
+A simple remote control for a thunder borg based on the [pyController](https://github.com/Schrotty/pyController) package.
+
+This package uses the 'Raspberry Pi Wireless USB Gamepad' from the official 'PiBorg' shop.
+
+For other controllers use the pyController package.
--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="monster-borg-remote-schrotty",
-    version="0.0.1",
+    name="thunder-remote",
+    version="1.0.1",
     author="Schrotty",
     author_email="rubenmaurer@live.de",
     description="A modified version of the 'pyController' package for usage with a ThunderBorg",
-    long_description="A modified version of the 'pyController' package for usage with a ThunderBorg",
+    long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/Schrotty/MonsterBorgRemote",
+    url="https://github.com/Schrotty/thunder-remote",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 2.7",
