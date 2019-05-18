1e8513d33f82291e198453855002ac206d0f191e
Ruben Maurer <rubenmaurer@live.de>
2019-05-18 19:33:27 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="thunder-remote",
-    version="0.3.8",
+    version="0.4.0",
     author="Schrotty",
     author_email="rubenmaurer@live.de",
     description="A modified version of the 'pyController' package for usage with a ThunderBorg",
