850bb0aabf589ad9dff5ebb8e22ffc3dfc2e66d2
Ruben Maurer <rubenmaurer@live.de>
2019-05-25 14:22:30 +0200

    WIP implement multiprocessing

--- __main__.py
+++ __main__.py
@@ -1,12 +1,24 @@
 from thunder_remote.RemoteControl import RemoteControl
 
 
-def wake_up(remote):
-    remote.control()
+def say_hello():
+    print "> Hello there!"
 
 
-def sleep(remote):
-    remote.sleep()
+def wake_up(rc):
+    print "> WAKE UP"
+
+    rc.wake()
+
+
+def sleep(rc):
+    print "> SLEEP"
+
+    rc.sleep()
+
+
+def kill_rc(rc):
+    rc.deactivate()
 
 
 def any(code):
@@ -14,15 +26,17 @@ def any(code):
 
 
 if __name__ == "__main__":
-    remote = RemoteControl(profile="madness", start_sleeping=True, debug_mode=False)
+    remote = RemoteControl(profile="default", start_sleeping=True, debug_mode=False)
     if remote.is_available():
         remote.events.wake_up += lambda: wake_up(remote)
         remote.events.on_west += lambda code, state: sleep(remote)
         remote.events.on_any += lambda code, state: any(state)
+        remote.events.on_select += lambda code, state: kill_rc(remote)
+        remote.events.on_north += lambda code, state: say_hello()
 
         remote.activate()
 
-    while True:
+    while remote.remote_online:
         remote.listen()
         if remote.is_sleeping:
             pass
